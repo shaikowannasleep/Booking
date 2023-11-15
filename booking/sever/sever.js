69bfb46b30c59954502cd904b08d630ea086c7d8
DinDun <dinhdung1110@gmail.com>
2023-11-15 20:56:34 +0700

    Sever editing

--- booking/sever/sever.js
+++ booking/sever/sever.js
@@ -1,1 +1,12 @@
-console.log("=======> Node Sever");
+const name ="RYAN"
+module.exports = {
+ name,
+};
+const express = require("express");
+
+const app = express();
+
+app.get("/api/:message", (req, res) => {
+     res.status(200).send(req.params.message);
+     });  
+app.listen(8000, () => console.log(`Sever is running on port 8000`));
