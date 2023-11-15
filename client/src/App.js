bca969e7ebb6f06c7d1e99c14a1e339627b44ebf
DinDun <dinhdung1110@gmail.com>
2023-11-15 19:30:25 +0700

    Recommit

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 // import those pages in App.js
 // then based on the path show each components using react-router components
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import React from "react";
 import Home from "./booking/Home";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 
 function App() {
   return (
-    <div>
-      <Home />
-      <Login />
-      <Register />
-
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
