8db7c92b13428f6efe988328b42730a4d7ce5121
DinDun <dinhdung1110@gmail.com>
2023-11-15 20:37:04 +0700

    Create redux
    npm i redux react-redux redux-devtools-extension

--- booking/client/src/App.js
+++ booking/client/src/App.js
@@ -1,20 +1,27 @@
+// import those pages in App.js
+// then based on the path show each components using react-router components
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./booking/Home";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import TopNav from "./components/TopNav";
 
+/**
+ * Lets create TopNavigation/menu bar so that we can easily TopNavigate between pages
+ * lets write it in App.js before we move it to its own component
+ */
+
 function App() {
   return (
-      <BrowserRouter>
+    <BrowserRouter>
       <TopNav />
-        <Switch>
-          <Route exact path ="/" component={Home} />
-          <Route exact path ="/login" component={Login} />
-          <Route exact path ="/register" component={Register} />
-        </Switch>
-      </BrowserRouter>
-      );
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
