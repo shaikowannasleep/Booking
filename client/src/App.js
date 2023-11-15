803eb9a64dd8e9a5df1bb5dbac9903ea2503b8d3
DinDun <dinhdung1110@gmail.com>
2023-11-15 17:49:09 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // import those pages in App.js
 // then based on the path show each components using react-router components
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from "react";
 import Home from "./booking/Home";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
